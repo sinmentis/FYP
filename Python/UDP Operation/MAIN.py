2806d671ea9df77cfa88d9e0baaff99ea246806f
sinmentis <sinmentis@foxmail.com>
2019-08-09 22:35:00 +1200

    Adding Fake Master testing program, pass multi-thread test!

--- Python/UDP Operation/MAIN.py
+++ Python/UDP Operation/MAIN.py
@@ -1,27 +1,50 @@
 import socket
-import time
 import select
 
-if __name__ == '__main__':
+"""=====================Head Define====================="""
+DEBUG_MODE = True
+UDP_RECEIVE_TIMEOUT = 1
+LOOP_DELAY = 1
+
+if DEBUG_MODE:
+    UDP_MASTER_IP = "127.0.0.2"
+    UDP_MASTER_PORT = 5005
+
+    UDP_PC_IP = "127.0.0.1"
+    UDP_PC_PORT = 5006
+else:
     UDP_MASTER_IP = "192.168.1.26"
+    UDP_MASTER_PORT = 5005
+
     UDP_PC_IP = "192.168.1.25"
-    UDP_PORT = 5005
-    print("UDP target IP:", UDP_MASTER_IP)
-    print("UDP target port:", UDP_PORT)
-    time.sleep(0.1)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP   
-    sock.settimeout(1)
-    sock.bind((UDP_PC_IP, UDP_PORT)) 
-    print("{}GO{}".format("*"*20,"*"*20))
-    
-    last_data = ""
-    
+    UDP_PC_PORT = 5005
+"""=====================Support functions====================="""
+
+
+def init_UDP_connection():
+    print("My IP is:     {0}, PORT: {1}".format(UDP_PC_IP, UDP_PC_PORT))
+    print("Target IP is: {0}, PORT: {1}\n\n".format(UDP_MASTER_IP, UDP_MASTER_PORT))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setblocking(0)
+    sock.bind((UDP_PC_IP, UDP_PC_PORT))
+
+    return sock
+
+
+"""===================== MAIN ====================="""
+
+
+if __name__ == '__main__':
+    sock = init_UDP_connection()
+    count = 0
     while True:
-        data, addr = sock.recvfrom(80)  # buffer size is 1024 bytes
-        if last_data != data:
+        ready = select.select([sock], [], [], UDP_RECEIVE_TIMEOUT)
+        if ready[0]:
+            data, _ = sock.recvfrom(80)  # buffer size is 1024 bytes
             print("PC: I just received message: [{0}]".format(data))
-            sock.sendto(bytes(data), (UDP_MASTER_IP, UDP_PORT))
+            sock.sendto(data, (UDP_MASTER_IP, UDP_MASTER_PORT))
             print("PC: I just Sent a [{0}]".format(data))
-        last_data = data
-        data = ""
-        time.sleep(2)
+        else:
+            print("{0}: Nothing Came through".format(count))
+            count += 1
