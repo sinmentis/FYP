31a417ef04189856488ad6bfbbc6f5130aeee154
Yikang Lin <yli252@canterbury.ac.nz>
2019-09-03 22:22:34 +1200

    Update Console

--- Python/GUI/GUI_MAIN.py
+++ Python/GUI/GUI_MAIN.py
@@ -8,35 +8,31 @@ class CounterGui:
     """======================Construct the GUI in the given window======================"""
 
     def __init__(self, window):
+        
+        # Assign windows to self
         self.window = window
 
-        self.init_GUI()  # setting title and geometry
-        self.add_menu()
-
-        self.count = 1
-        self.name_label = Label(window, text="Count:")
-        self.name_label.grid(row=0, column=0, sticky='E', padx=5, pady=5)
-
-        self.count_label = Label(window, text=str(self.count))
-        self.count_label.grid(row=0, column=1, sticky='W')
-
-        self.up_button = Button(window, text='up', command=lambda: self.change(+1))
-        self.up_button.grid(row=1, column=0, padx=5, pady=5)
-
-        self.down_button = Button(window, text='down', command=lambda: self.change(-1))
-        self.down_button.grid(row=1, column=1, padx=5, pady=5)
-
+        self.init_GUI()     # setting [title && geometry]
+        self.add_menu()     # Adding  [main menu && About us]
+        self.add_panel()    # Adding  [frame for panel && console].
+        self.add_console()
+        
+        # Start running GUI
         window.mainloop()
 
     """======================Draw the GUI======================"""
 
     def init_GUI(self):
+        # Set title
         self.window.title("Pilot training Emulator")
+        
+        # centralize the windows
         width_limit = (self.window.winfo_screenwidth() - 800) / 2
         height_limit = (self.window.winfo_screenheight() - 800) / 2
-        self.window.geometry("800x800+{0:.0f}+{1:.0f}".format(width_limit, height_limit))  # centralize the windows
+        self.window.geometry("800x800+{0:.0f}+{1:.0f}".format(width_limit, height_limit))  
+        
+        # Fix the width and height
         self.window.resizable(False, False)
-        # self.window.config(bg='#535352')
 
     def add_menu(self):
         # create a Top menu
@@ -56,12 +52,23 @@ class CounterGui:
         # Start the menu
         self.window.config(menu=menubar)
 
+
     def add_panel(self):
-        panel_frame = Frame(self.window)
+        self.panel_frame = tkinter.Frame(height = 600,width = 800, bg = '#C0C0C0')
+        self.panel_frame.pack(fill="x", padx=5, pady=5)
+        
+
 
     def add_console(self):
-        pass
-
+        
+        # Adding Frame with hw:200*800
+        self.console_frame = tkinter.Frame(height = 200,width = 800)
+        self.console_frame.pack(fill="x", padx=5, pady=5)
+        
+        # Adding message
+        self.console_message = Message(self.console_frame, text = "")
+        self.console_message.pack(side='left')
+    
     """======================Detect the event and react======================"""
 
     def change(self, delta):
@@ -70,11 +77,13 @@ class CounterGui:
         self.count_label['text'] = str(self.count)
 
     def test(self):
-        print("test")
+        self.update_message("HIHI")
 
     def about_us(self):
         showinfo(title="About us", message="This is E19")
-
+        
+    def update_message(self, message):
+        self.console_message.configure(text=message)
 
 if __name__ == "__main__":
     window = Tk()
--- Python/GUI/test_GUI.py
+++ Python/GUI/test_GUI.py
@@ -1,22 +1,33 @@
-import tkinter
+# -*- coding: cp936 -*-
 
-win = tkinter.Tk()
-win.title("yudanqu")
-win.geometry("400x400+200+50")
-
-frm = tkinter.Frame(win)
-frm.pack()
-
-# left
-frm_l = tkinter.Frame(frm)
-tkinter.Label(frm_l, text="宸︿笂", bg="pink").pack(side=tkinter.TOP)
-tkinter.Label(frm_l, text="宸︿笅", bg="blue").pack(side=tkinter.TOP)
-frm_l.pack(side=tkinter.LEFT)
-
-# right
-frm_r = tkinter.Frame(frm)
-tkinter.Label(frm_r, text="鍙充笂", bg="green").pack(side=tkinter.TOP)
-tkinter.Label(frm_r, text="鍙充笅", bg="red").pack(side=tkinter.TOP)
-frm_r.pack(side=tkinter.RIGHT)
-
-win.mainloop()
+from tkinter import *
+root = Tk()
+#以不同的颜色区别各个frame
+for fm in ['red','blue','yellow','green','white','black']:
+    #注意这个创建Frame的方法与其它创建控件的方法不同，第一个参数不是root
+    Frame(height = 20,width = 400,bg = fm).pack()
+root.mainloop()
+#添加不同颜色的Frame，大小均为20*400
+'''2.向Frame中添加Widget'''
+# -*- coding: cp936 -*-
+from Tkinter import *
+root = Tk()
+fm = []
+#以不同的颜色区别各个frame
+for color in ['red','blue']:
+    #注意这个创建Frame的方法与其它创建控件的方法不同，第一个参数不是root
+    fm.append(Frame(height = 200,width = 400,bg = color))
+#向下面的Frame中添加一个Label
+Label(fm[1],text = 'Hello label').pack()
+fm[0].pack()
+fm[1].pack()
+root.mainloop()
+#Label被添加到下面的Frame中了，而不是root默认的最上方。
+#大部分的方法来自gm,留到后面gm时再介绍
+'''3.Tk8.4以后Frame又添加了一类LabelFrame，添加了Title的支持'''
+from Tkinter import *
+root = Tk()
+for lf in ['red','blue','yellow']:
+    #可以使用text属性指定Frame的title
+    LabelFrame(height = 200,width = 300,text = lf).pack()
+root.mainloop()
