5d250aea572a19c8ec3795f855f7266de98d8e27
Yikang Lin <yli252@canterbury.ac.nz>
2019-09-03 23:01:23 +1200

    Console part Done

--- Python/GUI/GUI_MAIN.py
+++ Python/GUI/GUI_MAIN.py
@@ -8,15 +8,19 @@ class CounterGui:
     """======================Construct the GUI in the given window======================"""
 
     def __init__(self, window):
-        
+
         # Assign windows to self
         self.window = window
 
+        # gloabl variables
+        self.message = []
+
+        # Draw the GUI
         self.init_GUI()     # setting [title && geometry]
         self.add_menu()     # Adding  [main menu && About us]
         self.add_panel()    # Adding  [frame for panel && console].
         self.add_console()
-        
+
         # Start running GUI
         window.mainloop()
 
@@ -25,12 +29,12 @@ class CounterGui:
     def init_GUI(self):
         # Set title
         self.window.title("Pilot training Emulator")
-        
+
         # centralize the windows
         width_limit = (self.window.winfo_screenwidth() - 800) / 2
         height_limit = (self.window.winfo_screenheight() - 800) / 2
         self.window.geometry("800x800+{0:.0f}+{1:.0f}".format(width_limit, height_limit))  
-        
+
         # Fix the width and height
         self.window.resizable(False, False)
 
@@ -40,14 +44,14 @@ class CounterGui:
 
         # create main menu
         mainmenu = Menu(menubar, tearoff=False)
-        mainmenu.add_command(label='Setting', command=self.test)
-        mainmenu.add_command(label='Testing', command=self.test)
+        mainmenu.add_command(label='Setting', command=self.Menu_setting)
+        mainmenu.add_command(label='Testing', command=self.Menu_testing)
         mainmenu.add_separator()
         mainmenu.add_command(label='Exit', command=self.window.destroy)
         menubar.add_cascade(label='Menu', menu=mainmenu)
 
         # create "About us"
-        menubar.add_command(label='About us', command=self.about_us)
+        menubar.add_command(label='About us', command=self.Menu_about_us)
 
         # Start the menu
         self.window.config(menu=menubar)
@@ -56,36 +60,47 @@ class CounterGui:
     def add_panel(self):
         self.panel_frame = tkinter.Frame(height = 600,width = 800, bg = '#C0C0C0')
         self.panel_frame.pack(fill="x", padx=5, pady=5)
-        
 
 
     def add_console(self):
-        
+
         # Adding Frame with hw:200*800
         self.console_frame = tkinter.Frame(height = 200,width = 800)
         self.console_frame.pack(fill="x", padx=5, pady=5)
-        
+
         # Adding message
-        self.console_message = Message(self.console_frame, text = "")
+        self.message = []
+        self.console_message = Message(self.console_frame, text = "", width=800)
         self.console_message.pack(side='left')
-    
+
     """======================Detect the event and react======================"""
 
-    def change(self, delta):
-        """Handle clicks on one of the buttons"""
-        self.count += delta
-        self.count_label['text'] = str(self.count)
+    def update_message(self):
+        # Clear buffer limit the length to 10
+        if (len(self.message) > 10):
+            self.message = self.message[1:]
 
-    def test(self):
-        self.update_message("HIHI")
+        # Convert list into string
+        total_message = ""
+        for line in self.message:
+            total_message += line
 
-    def about_us(self):
+        # Update the message
+        self.console_message.configure(text=total_message)
+
+    """============= Menus ============="""
+    def Menu_testing(self):
+        self.message.append("Testing\n")
+        self.update_message()
+
+    def Menu_setting(self):
+        self.message.append("Setting\n")
+        self.update_message()
+
+    def Menu_about_us(self):
         showinfo(title="About us", message="This is E19")
-        
-    def update_message(self, message):
-        self.console_message.configure(text=message)
+
 
 if __name__ == "__main__":
     window = Tk()
     CounterGui(window)
-    window.mainloop()
